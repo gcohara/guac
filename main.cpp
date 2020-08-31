e5068c9a7372057be096d1c205b00739a0106d10
George O'Hara <georgeohara92@gmail.com>
2020-08-31 18:03:00 +0100

    outputting encoded files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <climits>
+#include <cstdint>
+#include <cstdlib>
 #include <deque>
 #include <functional>
 #include <iterator>
@@ -11,14 +13,18 @@
 #include <vector>
 #include <cstddef>
 #include <iostream>
+#include <fstream>
 
-using Byte = unsigned char;
+using Byte = std::uint8_t;
 using CharFreqHashMap = std::unordered_map<Byte, long long int>;
 using CharPriorQ = std::priority_queue<Byte, std::vector<Byte>, std::greater<Byte>>;
 using CodeLenMap = std::map<int, CharPriorQ>;
 using Codeword = std::deque<bool>;
 using EncodingBook = std::unordered_map<Byte, Codeword>;
 using DecodingBook = std::unordered_map<Codeword, Byte>;
+using EncodedMessage = std::deque<bool>;
+using ByteVec = std::vector<Byte>;
+using ByteDeq = std::deque<Byte>;
 
 
 // refactor to take references
@@ -47,10 +53,13 @@ CharFreqHashMap get_frequencies (std::string const & input);
 NodePriorQ build_priority_queue (CharFreqHashMap const & frequencies);
 Node * build_huffman_tree (NodePriorQ& pq);
 EncodingBook get_codebook_for_encoding(CodeLenMap& clm);
-std::vector<Byte> get_file_header(EncodingBook& ecb);
+std::deque<Byte> get_file_header(EncodingBook& ecb);
 void print_encoding_codebook(EncodingBook ecb);
 void increment_codeword(Codeword & cw);
+EncodedMessage encode_message(std::string const& message, EncodingBook& ecb);
 std::ostream &operator<<(std::ostream &os, Codeword const &cw);
+std::vector<Byte> convert_encoded_message_to_bytes(EncodedMessage em);
+void write_out_to_file(ByteDeq file_header, ByteVec encoded_message);
 
 
 
@@ -62,7 +71,7 @@ int main(int argc, char ** argv) {
     
     // open file
     // std::string const test = "oooh e ooh'?! ahaha ting tang walla walla bing bang"s;
-    std::string const test = "aabbbbcd";
+    std::string const test = "aabbbbcdaabbbbcdaabbbbcdaabbbbcdaabbbbcdaabbbbcdaabbbbcdaabbbbcd";
     std::cout << "Test string is " << test.length( )* 8 << " bits long.\n";
 
     auto frequencies = get_frequencies(test);
@@ -94,12 +103,67 @@ int main(int argc, char ** argv) {
     auto encoding_codebook = get_codebook_for_encoding(code_lens);
     print_encoding_codebook(encoding_codebook);
     auto fh = get_file_header(encoding_codebook);
-    std::cout << fh.size() << "\n";
-    for (auto itr = fh.rbegin(); itr != fh.rend(); itr++) {
-        std::cout << static_cast<int>(*itr) << " ";
-    }
-    std::cout << std::endl;
+    // std::cout << fh.size() << "\n";
+    // for (auto itr = fh.rbegin(); itr != fh.rend(); itr++) {
+    //     std::cout << static_cast<int>(*itr) << " ";
+    // }
+    // std::cout << std::endl;
+
+    auto encoded_message = encode_message(test, encoding_codebook);
+    // for ( auto& bit : encoded_message ) {
+    //     std::cout << bit;
+    // }
+    // std::cout << std::endl;
+    auto bm = convert_encoded_message_to_bytes(encoded_message);
+    // std::cout<<std::endl;
+    write_out_to_file(fh, bm);
     
+}
+
+void write_out_to_file(ByteDeq file_header, ByteVec encoded_message) {
+    std::ofstream ofs {"test.guac"};
+    if ( !ofs ) {
+        std::cout << "Could not create output file.\n";
+        exit(EXIT_FAILURE);
+    }
+    for ( auto b : file_header ) {
+        ofs << b;
+    }
+    for ( auto b : encoded_message ) {
+        ofs << b;
+    }
+    ofs.close();
+}
+
+
+// Important to note that the last byte is the number of unused trailing bits in
+// the penultimate byte.
+ByteVec convert_encoded_message_to_bytes(EncodedMessage em) {
+    std::vector<Byte> byte_message {};
+    while ( em.size() > 8 ) {
+        Byte temp = 0;
+        for (int i = 0; i < 8; i++) {
+            temp <<= 1;
+            temp +=em.front();
+            em.pop_front();
+        }
+        byte_message.push_back(temp);
+    }
+    Byte loose_bits = 8;
+    if ( em.size() == 0 ) {
+        loose_bits = 0;
+    }
+    Byte temp = 0;
+    while ( em.size() != 0 ) {
+        temp<<=1;
+        temp +=em.front();
+        em.pop_front();
+        loose_bits--;
+    }
+    temp <<= loose_bits;
+    byte_message.push_back(temp);
+    byte_message.push_back(loose_bits);
+    return byte_message;
 }
 
 std::ostream &operator<<(std::ostream &os, Codeword const &cw) {
@@ -109,8 +173,17 @@ std::ostream &operator<<(std::ostream &os, Codeword const &cw) {
     return os;
 }
 
-std::vector<Byte> get_file_header(EncodingBook& ecb) {
-    std::vector<Byte> file_header {};
+EncodedMessage encode_message(std::string const& message, EncodingBook& ecb) {
+    EncodedMessage enc_m {};
+    for ( auto c : message ) {
+        auto cw = ecb.at(c);
+        enc_m.insert(enc_m.end(), cw.begin(), cw.end());
+    }
+    return enc_m;
+}
+
+std::deque<Byte> get_file_header(EncodingBook& ecb) {
+    std::deque<Byte> file_header {};
     for (Byte b = 0; b < UCHAR_MAX; b++) {
         if (ecb.contains(b)) {
             file_header.push_back(ecb.at(b).size());
