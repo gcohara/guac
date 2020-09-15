59b176a380a899c4cbdf19c60d38207536a419ab
George O'Hara <georgeohara92@gmail.com>
2020-09-15 18:55:12 +0100

    added check that we are in root folder of project to testscript

--- testscript.sh
+++ testscript.sh
@@ -3,6 +3,11 @@
 ## Check that we are in the main guac folder
 ## how??
 
+if [ $(basename $PWD) != guac ]; then
+    echo "Please run testscript.sh from the guac root folder."
+    exit
+fi
+
 for file in ./testfiles/originals/*
 do
     sudo ./bin/guac --compress $file ./testfiles/compressed/$(basename $file)
@@ -19,5 +24,3 @@ for file in ./testfiles/decompressed/*
 do
     diff $file ./testfiles/originals/$(basename $file)
 done
-
-cd ../../
